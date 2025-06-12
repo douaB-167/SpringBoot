ddb41bbfd1119d058f63238cb3b8b91270d8550d
douaB-167 <doua.boufqir@edu.immaginazioneelavoro.it>
2025-06-12 11:26:03 +0200

    Add files via upload

---
+++ HelloSpring/src/test/java/com/MyAzienda/HelloSpring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyAzienda.HelloSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
